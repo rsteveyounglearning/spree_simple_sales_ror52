beaec339b5851a6368e2e8c53dd0f97d07d7909c
R. Steve Young <r.steve.young@gmail.com>
2020-06-03 00:04:30 -0400

    Fixing scale param in migration.

--- db/migrate/20200602221658_add_sale_price_to_spree_variants.rb
+++ db/migrate/20200602221658_add_sale_price_to_spree_variants.rb
@@ -1,5 +1,5 @@
 class AddSalePriceToSpreeVariants < SpreeExtension::Migration
   def change
-    add_column :spree_variants, :sale_price, :decimal, precision: 8, scale:
+    add_column :spree_variants, :sale_price, :decimal, precision: 8, scale: 2
   end
 end
