65a0198ca37eb22d4b14045fa9eca035231a8352
R. Steve Young <r.steve.young@gmail.com>
2020-06-03 00:50:18 -0400

    Decorating Spree::Variant model with new 'price_in' method.

---
+++ app/models/spree_simple_sales_ror52/spree/variant_decorator.rb
@@ -0,0 +1,10 @@
+module SpreeSimpleSalesRor52
+  module Spree
+    module VariantDecorator
+      def price_in(currency)
+        return super unless sale_price.present?
+        ::Spree::Price.new(variant_id: self.id, amount: self.sale_price, currency: currency)
+      end
+    end
+  end
+end
