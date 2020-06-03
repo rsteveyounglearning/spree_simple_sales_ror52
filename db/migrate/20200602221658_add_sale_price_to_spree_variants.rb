5051b9b76994705c6d03215ad3e0871615be879a
R. Steve Young <r.steve.young@gmail.com>
2020-06-03 00:11:20 -0400

    Updating migration parent class in order to use ActiveRecord::Migration[5.0] instead SpreeExtension::Migration.

--- db/migrate/20200602221658_add_sale_price_to_spree_variants.rb
+++ db/migrate/20200602221658_add_sale_price_to_spree_variants.rb
@@ -1,4 +1,4 @@
-class AddSalePriceToSpreeVariants < SpreeExtension::Migration
+class AddSalePriceToSpreeVariants < ActiveRecord::Migration[5.0]
   def change
     add_column :spree_variants, :sale_price, :decimal, precision: 8, scale: 2
   end
